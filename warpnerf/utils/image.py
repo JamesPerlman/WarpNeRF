cfd2b421c5084b9142d64a6eeb1cb996ba24c56c
JamesPerlman <jam.e.perl@gmail.com>
2024-08-18 18:34:57 -0700

    add assertion, save_image

--- warpnerf/utils/image.py
+++ warpnerf/utils/image.py
@@ -8,7 +8,14 @@ def is_image(path: Path) -> bool:
     return path.suffix.lower() in [".jpg", ".jpeg", ".png", ".bmp", ".tiff"]
 
 def load_image(path: Path) -> wp.array(dtype=float):
+    assert is_image(path), f"{path} is not an image file"
     img = Image.open(path)
     img = np.array(img)
     img = img.astype(np.float32) / 255.0
     return wp.from_numpy(img, dtype=wp.float32)
+
+def save_image(data: wp.array(dtype=float), path: Path):
+    img = np.array(data.to("cpu"))
+    img = (img * 255.0).astype(np.uint8)
+    img = Image.fromarray(img)
+    img.save(path)
