30af77560a1bb30253cfb0f64fbc620e2a6dc937
JamesPerlman <jam.e.perl@gmail.com>
2024-08-19 16:30:19 -0700

    rename bundler load cmd

--- warpnerf/cli/__init__.py
+++ warpnerf/cli/__init__.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 import tyro
 
-from warpnerf.utils.bundler_sfm import read_bundler_sfm_data
+from warpnerf.utils.bundler_sfm import create_bundler_sfm_data_from_path
 from warpnerf.utils.reality_capture import run_reality_capture
 
-
 def open_bundler_file(path: Path):
-    data = read_bundler_sfm_data(path)
+    data = create_bundler_sfm_data_from_path(path)
 
 def main():
     tyro.extras.subcommand_cli_from_dict(
