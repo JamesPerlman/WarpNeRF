11d0c6388b25b6449665ce34201b3ea066d0eba1
James <james.perlman@dream-ai.com>
2024-08-27 10:27:41 -0700

    add to README

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Installation:
 
 ```
 conda install -c http://fvdb.huangjh.tech:8000/t/bfIk3ltPmB8tjSGgupFjwVXuumhfClpi/get/early-access -c pytorch -c nvidia fvdb
+pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 cd external/nvdiffrast && pip install .
 ```
--- warpnerf/models/tri_mip_encoding.py
+++ warpnerf/models/tri_mip_encoding.py
@@ -1,98 +1,62 @@
 import numpy as np
 import warp as wp
 
-@wp.struct
-class MipEncoding3D:
-    n_levels: wp.int32
-    n_channels: wp.int32
-    base_res: wp.int32
-    base_radius: wp.float32
-    data_xy: wp.array3d(dtype=wp.float32)
-    data_yz: wp.array3d(dtype=wp.float32)
-    data_xz: wp.array3d(dtype=wp.float32)
+import nvdiffrast.torch
+import torch
 
 
-def create_mip_encoding3D(n_levels: wp.int32, n_channels: wp.int32, base_res: wp.int32, device: str = "cuda") -> MipEncoding3D:
+class TriMipEncoding(torch.nn.Module):
+    def __init__(
+        self,
+        n_levels: int,
+        base_resolution: int,
+        feature_dim: int,
+    ):
+        super(TriMipEncoding, self).__init__()
+        self.n_levels = n_levels
+        self.base_resolution = base_resolution
+        self.feature_dim = feature_dim
 
-    res_f = wp.float32(base_res)
-    base_radius = wp.sqrt(1.0 / (wp.pi * res_f * res_f))
-    
-    encoding = MipEncoding3D()
-    encoding.n_levels = n_levels
-    encoding.n_channels = n_channels
-    encoding.base_res = base_res
-    encoding.base_radius = base_radius
-    encoding.data_xy = wp.array(np.random.rand(n_channels, base_res, base_res), dtype=wp.float32, device=device)
-    encoding.data_yz = wp.array(np.random.rand(n_channels, base_res, base_res), dtype=wp.float32, device=device)
-    encoding.data_xz = wp.array(np.random.rand(n_channels, base_res, base_res), dtype=wp.float32, device=device)
-
-
-@wp.func
-def get_mip_level_for_radius(encoding: MipEncoding3D, radius: wp.float32) -> wp.float32:
-
-    return wp.float32(wp.log2(encoding.base_radius / radius))
-
-
-@wp.func
-def get_nearest_mip2D_pixels(
-    data: wp.array3d(dtype=wp.float32),
-    channel: wp.int32,
-    base_res: wp.int32,
-    level: wp.int32,
-    x: wp.float32,
-    y: wp.float32
-) -> wp.float32:
-    
-    base_res = wp.float32(base_res)
-    level_res = wp.float32(base_res / (2 ** level))
-    
-    # Calculate the region in the base level that corresponds to the requested mip level pixel
-    x_start = wp.int32(wp.clamp(x * base_res / level_res, 0.0, base_res - 1.0))
-    y_start = wp.int32(wp.clamp(y * base_res / level_res, 0.0, base_res - 1.0))
-    
-    x_end = wp.int32(wp.clamp((x + 1.0) * base_res / level_res, 0.0, base_res))
-    y_end = wp.int32(wp.clamp((y + 1.0) * base_res / level_res, 0.0, base_res))
-
-    # Average the pixels in this region
-    pixel_sum = wp.float32(0.0)
-    count = (x_end - x_start) * (y_end - y_start)
-
-    if count == 0.0:
-        return 0.0
-
-    for j in range(y_start, y_end):
-        for i in range(x_start, x_end):
-            pixel_sum += data[channel][j][i]
-
-    return pixel_sum / count
-
-
-@wp.func
-def get_mip3D_pixel(
-    encoding: MipEncoding3D,
-    channel: wp.int32,
-    pos: wp.vec3f,
-    radius: wp.float32
-) -> wp.vec3f:
-    
-    level = get_mip_level_for_radius(encoding, radius)
-    lower_level = wp.int32(level)
-    upper_level = wp.max(lower_level + 1, encoding.n_levels - 1)
-
-    values = wp.empty(encoding.n_channels, dtype=wp.vec3f)
-
-    for i in range(encoding.n_channels):
-        lower_value = wp.vec3f(
-            get_mip2D_pixel(encoding.data_xy, i, encoding.base_res, lower_level, pos.x, pos.y),
-            get_mip2D_pixel(encoding.data_yz, i, encoding.base_res, lower_level, pos.y, pos.z),
-            get_mip2D_pixel(encoding.data_xz, i, encoding.base_res, lower_level, pos.x, pos.z)
+        self.register_parameter(
+            "texture",
+            torch.nn.parameter(torch.zeros(3, base_resolution, base_resolution, feature_dim)),
         )
 
-        upper_value = wp.vec3f(
-            get_mip2D_pixel(encoding.data_xy, i, encoding.base_res, lower_level, pos.x, pos.y),
-            get_mip2D_pixel(encoding.data_yz, i, encoding.base_res, lower_level, pos.y, pos.z),
-            get_mip2D_pixel(encoding.data_xz, i, encoding.base_res, lower_level, pos.x, pos.z)
-        ) if lower_level != upper_level else lower_value
+        torch.nn.init.uniform_(self.texture, -1e-2, 1e-2)
 
-        # Interpolate between the two mip levels
-        t = level - wp.float32(lower_level)
+
+    def forward(
+            self,
+            x, # normalized to [0, 1]
+            level # in [0, n_levels - 1]
+        ):
+
+        if x.shape[0] == 0:
+            return torch.zeros([0, self.feature_dim * 3], device=x.device)
+        
+        x_decomposed = torch.stack(
+            [
+                x[:, None, [1, 2]],
+                x[:, None, [0, 2]],
+                x[:, None, [0, 1]],
+            ],
+            dim=0,
+        )
+
+        level = torch.stack([level, level, level], dim=0)
+
+        level = torch.broadcast_to(level, x_decomposed.shape[:3]).contiguous()
+
+        # shape is 3xNx1xC
+        enc = nvdiffrast.torch.texture(
+            tex=self.texture,
+            uv=x_decomposed,
+            mip_level_bias=level,
+            boundary_mode="clamp",
+            max_mip_level=self.n_levels - 1,
+        )
+
+        # shape is Nx(3C)
+        enc = enc.permute(1, 2, 0, 3).contiguous().view(x.shape[0], self.feature_dim * 3)
+
+        return enc
