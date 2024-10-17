0611b91fd13e9400d2c956b2ffb0c42acd8fb60a
James <james.perlman@dream-ai.com>
2024-10-17 16:21:06 -0700

    add NeRFModel

---
+++ warpnerf/models/nerf_model.py
@@ -0,0 +1,13 @@
+import torch
+from fvdb import GridBatch
+from warpnerf.models.mlp import MLP
+
+class NeRFModel(torch.nn.Module):
+    grid: GridBatch
+    dir_enc: torch.nn.Module
+    pos_enc: torch.nn.Module
+    mlp_base: MLP
+    mlp_head: MLP
+
+    def __init__() -> None:
+        super().__init__()
