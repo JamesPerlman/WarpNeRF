a1aceddca694c45c65bc12036c8e7a9d77749a92
James <james.perlman@dream-ai.com>
2024-10-17 16:23:20 -0700

    add TruncExp function

---
+++ warpnerf/utils/trunc_exp.py
@@ -0,0 +1,14 @@
+import torch
+
+class TruncExp(torch.autograd.Function):  # pylint: disable=abstract-method
+    # Implementation from torch-ngp:
+    # https://github.com/ashawkey/torch-ngp/blob/93b08a0d4ec1cc6e69d85df7f0acdfb99603b628/activation.py
+    @staticmethod
+    def forward(ctx, x):
+        ctx.save_for_backward(x)
+        return torch.exp(x)
+
+    @staticmethod
+    def backward(ctx, g):
+        x = ctx.saved_tensors[0]
+        return g * torch.exp(torch.clamp(x, min=-15, max=15))
