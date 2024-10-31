c29e2b0104ef56a624ac762f4785c1cfc81a858f
James <james.perlman@dream-ai.com>
2024-10-31 15:54:05 -0700

    fix ray math

--- warpnerf/utils/ray.py
+++ warpnerf/utils/ray.py
@@ -12,9 +12,14 @@ def get_global_ray_at_pixel_xy(
     pixel_y: wp.int32,
     near: wp.float32 = 0.05,
 ) -> Ray:
+    wf = wp.float32(img_w)
+    hf = wp.float32(img_h)
+    xf = wp.float32(pixel_x)
+    yf = wp.float32(pixel_y)
+
     v = wp.vec3f(
-        (wp.float32(pixel_x) - wp.float32(img_w) / 2.0 + 0.5) / camera.f,
-        (wp.float32(pixel_y) - wp.float32(img_h) / 2.0 + 0.5) / camera.f,
+        ((xf + 0.5) / wf - 0.5) * camera.sx / camera.f,
+        ((yf + 0.5) / hf - 0.5) * camera.sy / camera.f,
         1.0
     )
 
@@ -27,8 +32,8 @@ def get_global_ray_at_pixel_xy(
     ray.cos = v_norm.z
 
     # radius is half way between the apothem and circumradius of the pixel
-    pixel_w = camera.sx / wp.float32(img_w)
-    pixel_h = camera.sy / wp.float32(img_h)
+    pixel_w = camera.sx / wf
+    pixel_h = camera.sy / hf
     pixel_circumradius = wp.sqrt(pixel_w * pixel_w + pixel_h * pixel_h) / 2.0
     pixel_avg_apothem = (pixel_w + pixel_h) / 4.0
     ray.radius = 0.5 * (pixel_avg_apothem + pixel_circumradius)
