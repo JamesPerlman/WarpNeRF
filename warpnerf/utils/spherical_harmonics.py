55447f613202d1d75f48ba794d0cd01daf609b12
James <james.perlman@dream-ai.com>
2024-11-24 12:43:43 -0800

    add spherical harmonics utils

---
+++ warpnerf/utils/spherical_harmonics.py
@@ -0,0 +1,160 @@
+import torch
+
+def evaluate_sh_bases(basis_dim: int, dirs: torch.Tensor):
+    """
+    Evaluate spherical harmonics bases at unit directions,
+    without taking linear combination.
+    At each point, the final result may the be
+    obtained through simple multiplication.
+
+    :param basis_dim: int SH basis dim. Currently, 1-25 square numbers supported
+    :param dirs: torch.Tensor (..., 3) unit directions
+
+    :return: torch.Tensor (..., basis_dim)
+    """
+    SH_C0 = 0.28209479177387814
+    SH_C1 = 0.4886025119029199
+    SH_C2 = [
+        1.0925484305920792,
+        -1.0925484305920792,
+        0.31539156525252005,
+        -1.0925484305920792,
+        0.5462742152960396
+    ]
+    SH_C3 = [
+        -0.5900435899266435,
+        2.890611442640554,
+        -0.4570457994644658,
+        0.3731763325901154,
+        -0.4570457994644658,
+        1.445305721320277,
+        -0.5900435899266435
+    ]
+    SH_C4 = [
+        2.5033429417967046,
+        -1.7701307697799304,
+        0.9461746957575601,
+        -0.6690465435572892,
+        0.10578554691520431,
+        -0.6690465435572892,
+        0.47308734787878004,
+        -1.7701307697799304,
+        0.6258357354491761,
+    ]
+    result = torch.empty((*dirs.shape[:-1], basis_dim), dtype=dirs.dtype, device=dirs.device)
+    result[..., 0] = SH_C0
+
+    if basis_dim > 1:
+        x, y, z = dirs.unbind(-1)
+        result[..., 1] = -SH_C1 * y
+        result[..., 2] = SH_C1 * z
+        result[..., 3] = -SH_C1 * x
+    
+        if basis_dim > 4:
+            xx, yy, zz = x * x, y * y, z * z
+            xy, yz, xz = x * y, y * z, x * z
+            result[..., 4] = SH_C2[0] * xy
+            result[..., 5] = SH_C2[1] * yz
+            result[..., 6] = SH_C2[2] * (2.0 * zz - xx - yy)
+            result[..., 7] = SH_C2[3] * xz
+            result[..., 8] = SH_C2[4] * (xx - yy)
+
+            if basis_dim > 9:
+                result[..., 9] = SH_C3[0] * y * (3 * xx - yy)
+                result[..., 10] = SH_C3[1] * xy * z
+                result[..., 11] = SH_C3[2] * y * (4 * zz - xx - yy)
+                result[..., 12] = SH_C3[3] * z * (2 * zz - 3 * xx - 3 * yy)
+                result[..., 13] = SH_C3[4] * x * (4 * zz - xx - yy)
+                result[..., 14] = SH_C3[5] * z * (xx - yy)
+                result[..., 15] = SH_C3[6] * x * (xx - 3 * yy)
+
+                if basis_dim > 16:
+                    result[..., 16] = SH_C4[0] * xy * (xx - yy)
+                    result[..., 17] = SH_C4[1] * yz * (3 * xx - yy)
+                    result[..., 18] = SH_C4[2] * xy * (7 * zz - 1)
+                    result[..., 19] = SH_C4[3] * yz * (7 * zz - 3)
+                    result[..., 20] = SH_C4[4] * (zz * (35 * zz - 30) + 3)
+                    result[..., 21] = SH_C4[5] * xz * (7 * zz - 3)
+                    result[..., 22] = SH_C4[6] * (xx - yy) * (7 * zz - 1)
+                    result[..., 23] = SH_C4[7] * xz * (xx - 3 * yy)
+                    result[..., 24] = SH_C4[8] * (xx * (xx - 3 * yy) - yy * (3 * xx - yy))
+                
+    return result
+
+def evaluate_sh(deg: int, sh: torch.Tensor, dirs: torch.Tensor):
+    C0 = 0.28209479177387814
+    C1 = 0.4886025119029199
+    C2 = [
+        1.0925484305920792,
+        -1.0925484305920792,
+        0.31539156525252005,
+        -1.0925484305920792,
+        0.5462742152960396
+    ]
+    C3 = [
+        -0.5900435899266435,
+        2.890611442640554,
+        -0.4570457994644658,
+        0.3731763325901154,
+        -0.4570457994644658,
+        1.445305721320277,
+        -0.5900435899266435
+    ]
+    C4 = [
+        2.5033429417967046,
+        -1.7701307697799304,
+        0.9461746957575601,
+        -0.6690465435572892,
+        0.10578554691520431,
+        -0.6690465435572892,
+        0.47308734787878004,
+        -1.7701307697799304,
+        0.6258357354491761,
+    ]
+
+    # sh is a tensor of shape [N, C, (deg+1)**2]
+    # dirs is a tensor of shape [N, 3]
+    assert 0 <= deg <= 4
+    assert (deg + 1) ** 2 == sh.shape[-1]
+    # C = sh.shape[-2]
+
+    result = C0 * sh[..., 0]
+    if deg > 0:
+        x, y, z = dirs[..., 0:1], dirs[..., 1:2], dirs[..., 2:3]
+        result = (result -
+                C1 * y * sh[..., 1] +
+                C1 * z * sh[..., 2] -
+                C1 * x * sh[..., 3])
+
+        if deg > 1:
+            xx, yy, zz = x * x, y * y, z * z
+            xy, yz, xz = x * y, y * z, x * z
+            result = (result +
+                    C2[0] * xy * sh[..., 4] +
+                    C2[1] * yz * sh[..., 5] +
+                    C2[2] * (2.0 * zz - xx - yy) * sh[..., 6] +
+                    C2[3] * xz * sh[..., 7] +
+                    C2[4] * (xx - yy) * sh[..., 8])
+
+            if deg > 2:
+                result = (result +
+                        C3[0] * y * (3 * xx - yy) * sh[..., 9] +
+                        C3[1] * xy * z * sh[..., 10] +
+                        C3[2] * y * (4 * zz - xx - yy)* sh[..., 11] +
+                        C3[3] * z * (2 * zz - 3 * xx - 3 * yy) * sh[..., 12] +
+                        C3[4] * x * (4 * zz - xx - yy) * sh[..., 13] +
+                        C3[5] * z * (xx - yy) * sh[..., 14] +
+                        C3[6] * x * (xx - 3 * yy) * sh[..., 15])
+            
+                if deg > 3:
+                    result = (result + C4[0] * xy * (xx - yy) * sh[..., 16] +
+                            C4[1] * yz * (3 * xx - yy) * sh[..., 17] +
+                            C4[2] * xy * (7 * zz - 1) * sh[..., 18] +
+                            C4[3] * yz * (7 * zz - 3) * sh[..., 19] +
+                            C4[4] * (zz * (35 * zz - 30) + 3) * sh[..., 20] +
+                            C4[5] * xz * (7 * zz - 3) * sh[..., 21] +
+                            C4[6] * (xx - yy) * (7 * zz - 1) * sh[..., 22] +
+                            C4[7] * xz * (xx - 3 * yy) * sh[..., 23] +
+                            C4[8] * (xx * (xx - 3 * yy) - yy * (3 * xx - yy)) * sh[..., 24])
+
+    return result
